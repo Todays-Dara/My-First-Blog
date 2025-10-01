5d76df5b4810a184bb19d0b7620549b5bba6b9a7
Darasimi <daraaisola@gmail.com>
2025-10-01 10:54:02 +0100

    Modified templates to display different posts from Database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import Post
 # Create your views here.
 def post_list(request):
-    return render(request,'blog/post_list.html', {})
+    me = User.objects.get(username='Darasimi')
+    post = Post.objects.filter(author=me)
+    return render(request,'blog/post_list.html', {"post":post})
